a9b449eacf42d831b8fe368440b2f045dd714d09
JohnsMTCOTest <johnsonmtco@gmail.com>
2025-10-15 18:02:46 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Render Test", layout="wide")
+st.title("✅ Render is Working!")
+st.write("If you can see this, your Streamlit server is connected and running.")
